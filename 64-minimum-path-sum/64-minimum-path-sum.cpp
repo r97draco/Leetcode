a5c4f1c807694bd33073c12c4968ba9d220f0bd8
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2022-08-15 15:17:27 -0600

    Time: 18 ms (37.89%), Space: 10.2 MB (29.24%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -10,28 +10,9 @@ public:
         int r = g[m][n]+ rec(g, m, n-1, dp);
         return dp[m][n]= min(l , r);
     }
-    int MinPathSum(vector<vector<int>>& g) {
-        int m=g.size(), n= g[0].size();
-        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        return rec(g, m-1, n-1, dp);
-    }
-
     int minPathSum(vector<vector<int>>& g) {
         int m=g.size(), n= g[0].size();
         vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        dp[0][0]= g[0][0];
-        for(int i=1; i<n; i++){
-            dp[0][i]= g[0][i]+ dp[0][i-1];
-        }
-        for(int i=1; i<m; i++){
-            dp[i][0]= g[i][0]+ dp[i-1][0];
-        }
-        
-        for(int  i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j]=g[i][j]+ min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[m-1][n-1];
+        return rec(g, m-1, n-1, dp);
     }
 }; 
