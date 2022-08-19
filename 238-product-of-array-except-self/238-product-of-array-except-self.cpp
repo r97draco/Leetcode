7408b558f9629994d9a100db4613cf74f006b31d
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2022-08-19 00:15:33 -0600

    Time: 62 ms (6.75%), Space: 24.4 MB (51.01%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -4,7 +4,7 @@ public:
         int sz = n.size(); 
         vector<int> lef(sz, 1);
         vector<int> right(sz, 1);
-        lef[0]=1; right[sz-1]=1;
+        // lef[0]=1; right[sz-1]=1;
         
         for(int i=0; i< sz-1; i++) {
             lef[i+1] = lef[i]*n[i];
