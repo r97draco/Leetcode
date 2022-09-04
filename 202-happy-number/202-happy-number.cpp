1552ac12d7ac1f93ac471893153936e5aa4efe4d
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2022-09-03 18:52:21 -0600

    Time: 4 ms (46.60%), Space: 6 MB (63.98%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
-    set<int> dp;
-    bool isHappy(int n) {
-        if(dp.find(n)!=dp.end())return false;
-        dp.insert(n);
-        if(n==1)return true;
+    // set<int> dp;
+    // bool isHappy(int n) {
+    //     if(dp.find(n)!=dp.end())return false;
+    //     dp.insert(n);
+    //     if(n==1)return true;
+    //     int sum=0;
+    //     while(n!=0){
+    //         int r= n%10;
+    //         sum+= r*r;
+    //         n/=10;
+    //     }
+    //     return isHappy(sum);
+    // }
+    
+    bool isHappy(int n){
+        if(n==1)return 1;
+        if(n==89)return 0;
         int sum=0;
         while(n!=0){
             int r= n%10;
