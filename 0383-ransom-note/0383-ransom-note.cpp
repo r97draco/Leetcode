dbcc8eb7ed958a68c555c1ecec9f8fe131979dac
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2022-12-08 00:04:24 -0600

    Time: 67 ms (15.52%), Space: 9 MB (6.53%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int> ransom_map, mag_map;
-        for(auto i: ransomNote){
-            ransom_map[i]++;
-        }
+        map<char,int> mag_map;
         for(auto i: magazine){
             mag_map[i]++;
         }
-        for(auto i: ransom_map){
-            char s= i.first; int val= i.second;
-            auto tmp=mag_map.find(s);
-            if(tmp!= mag_map.end()){
-                // auto tmp= mag_map[s];
-                if(i.second> tmp->second)return false;
-            }
-            else return false;
+        for(auto i: ransomNote){
+            mag_map[i]--;
+            if(mag_map[i]<0)return false;
         }
+        
+        // for(auto i: ransom_map){
+        //     char s= i.first; int val= i.second;
+        //     auto tmp=mag_map.find(s);
+        //     if(tmp!= mag_map.end()){
+        //         // auto tmp= mag_map[s];
+        //         if(i.second> tmp->second)return false;
+        //     }
+        //     else return false;
+        // }
         return true;
     }
 };
