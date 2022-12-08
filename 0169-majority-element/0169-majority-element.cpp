0814212ab41a6de57aad28019e0fc91fd4ea7f3a
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2022-12-08 00:54:46 -0600

    Time: 36 ms (63.79%), Space: 19.5 MB (61.04%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int sz= nums.size();
+        map<int,int> hash;
+        for(auto i: nums){
+            hash[i]++;
+            if(hash[i]> sz/2)return i;
+        }
+        return 0;
+    }
+};
