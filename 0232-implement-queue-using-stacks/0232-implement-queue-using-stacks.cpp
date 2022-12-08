2c8a929086baa96231fe3f546b4fa31b2e36be5f
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2022-12-07 23:14:07 -0600

    Time: 3 ms (47.98%), Space: 7 MB (89.65%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -6,34 +6,40 @@ public:
     }
     
     void push(int x) {
-       out.push(x); 
-       stack<int> tmp = out;
-       in= stack<int>();
-       while(!tmp.empty()){
-           in.push(tmp.top());
-           tmp.pop();
-       }
+       in.push(x); 
+       // stack<int> tmp = out;
+       // in= stack<int>();
+       // while(!tmp.empty()){
+       //     in.push(tmp.top());
+       //     tmp.pop();
+       // }
        
     }
     
     int pop() {
-        int a= in.top();
-        in.pop(); 
-        stack<int> tmp = in;
-        out= stack<int>();
-        while(!tmp.empty()){
-           out.push(tmp.top());
-           tmp.pop();
-       }
+        int a= peek();
+        out.pop(); 
+       //  stack<int> tmp = in;
+       //  out= stack<int>();
+       //  while(!tmp.empty()){
+       //     out.push(tmp.top());
+       //     tmp.pop();
+       // }
        return a;
     }
     
     int peek() {
-        return in.top();
+        if(out.empty()){
+            while(in.size()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
     }
     
     bool empty() {
-        return in.size()==0? true: false;
+        return in.size()==0 && out.size()==0;
     }
 };
 
