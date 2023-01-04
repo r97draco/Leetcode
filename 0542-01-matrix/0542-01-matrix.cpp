1d0157fae2411c6e2fbb0cfce55750f75afeba93
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-01-03 23:51:21 -0600

    Time: 147 ms (52.69%), Space: 27.3 MB (98.70%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<vector<int>> visited;
+    
+    int dfs(vector<vector<int>>& mat, int i, int j){
+        if(i>=0 && j>=0 && i<mat.size() && j<mat[0].size()){
+            if(mat[i][j] == 0)
+                return 0;
+            if(mat[i][j]>=1){
+                mat[i][j]=-1;
+                int l,r,u,d;
+                l=1+dfs(mat, i-1, j);
+                r=1+dfs(mat, i+1, j);
+                u=1+dfs(mat, i, j-1);
+                d=1+dfs(mat, i, j+1);
+                mat[i][j]=1;
+                return min(l,min(r, min(u,d)));
+                
+            }
+        }
+        return 100;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        // int m= mat.size(), n=mat[0].size();
+        // vector<vector<int>> res(m, vector<int> (n, 0));
+        // for(int i=0; i<m; i++){
+        //     res[i][0]= mat[i][0];
+        //     res[i][m-1]= mat[i][m-1];
+        // }
+        // for(int i=0; i<n; i++){
+        //     res[0][i]= mat[0][i];
+        //     res[n-1][i]=mat[n-1][i];
+        // }
+        
+        
+        int m = mat.size(), n = mat[0].size(), INF = m + n; // The distance of cells is up to (M+N)
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] == 0) continue;
+                int top = INF, left = INF;
+                if (r - 1 >= 0) top = mat[r - 1][c];
+                if (c - 1 >= 0) left = mat[r][c - 1];
+                mat[r][c] = min(top, left) + 1;
+            }
+        }
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                int bottom = INF, right = INF;
+                if (r + 1 < m) bottom = mat[r + 1][c];
+                if (c + 1 < n) right = mat[r][c + 1];
+                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
+            }
+        }
+        return mat;
+        return mat;
+    }
+};
