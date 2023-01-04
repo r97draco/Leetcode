55d5c7d53fc4c00e898802c1570df4fffc142b2f
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-01-04 00:02:59 -0600

    Time: 141 ms (54.03%), Space: 30.2 MB (66.14%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -21,8 +21,8 @@ public:
         return 100;
     }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        // int m= mat.size(), n=mat[0].size();
-        // vector<vector<int>> res(m, vector<int> (n, 0));
+        int m= mat.size(), n=mat[0].size(), INF=m+n;
+        vector<vector<int>> res(m, vector<int> (n, 0));
         // for(int i=0; i<m; i++){
         //     res[i][0]= mat[i][0];
         //     res[i][m-1]= mat[i][m-1];
@@ -33,38 +33,44 @@ public:
         // }
         
         
-//         for(int i=1; i<m; i++){
-//             for(int j=1; j<n; j++){
-//                 if(mat[i][j]==0)continue;
-//                 mat[i][j]= min(mat[i-1][j], mat[i][j-1]);
-//             }
-//         }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0)continue;
+                int top=INF, left= INF;
+                if(i>0) top= mat[i-1][j];
+                if(j>0) left= mat[i][j-1];
+                mat[i][j]= min(top,left)+1;
+            }
+        }
         
-//         for(int i=m-2; i>=0; i--){
-//             for(int j=n-2; j>=0; j--){
-//                 if(mat[i][j]==0)continue;
-//                 mat[i][j]=min(mat[i][j], 1+ (mat[i+1][j],mat[i][j+1]));
-//             }
-//         }
-              int m = mat.size(), n = mat[0].size(), INF = m + n; // The distance of cells is up to (M+N)
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (mat[r][c] == 0) continue;
-                int top = INF, left = INF;
-                if (r - 1 >= 0) top = mat[r - 1][c];
-                if (c - 1 >= 0) left = mat[r][c - 1];
-                mat[r][c] = min(top, left) + 1;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(mat[i][j]==0)continue;
+                int bot=INF, rig= INF;
+                if(i<m-1) bot= mat[i+1][j];
+                if(j<n-1) rig= mat[i][j+1];
+                mat[i][j]=min(mat[i][j], 1+min (bot,rig));
             }
         }
-        for (int r = m - 1; r >= 0; r--) {
-            for (int c = n - 1; c >= 0; c--) {
-                if (mat[r][c] == 0) continue;
-                int bottom = INF, right = INF;
-                if (r + 1 < m) bottom = mat[r + 1][c];
-                if (c + 1 < n) right = mat[r][c + 1];
-                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
-            }
-        }
+        //       int m = mat.size(), n = mat[0].size(), INF = m + n; // The distance of cells is up to (M+N)
+        // for (int r = 0; r < m; r++) {
+        //     for (int c = 0; c < n; c++) {
+        //         if (mat[r][c] == 0) continue;
+        //         int top = INF, left = INF;
+        //         if (r - 1 >= 0) top = mat[r - 1][c];
+        //         if (c - 1 >= 0) left = mat[r][c - 1];
+        //         mat[r][c] = min(top, left) + 1;
+        //     }
+        // }
+        // for (int r = m - 1; r >= 0; r--) {
+        //     for (int c = n - 1; c >= 0; c--) {
+        //         if (mat[r][c] == 0) continue;
+        //         int bottom = INF, right = INF;
+        //         if (r + 1 < m) bottom = mat[r + 1][c];
+        //         if (c + 1 < n) right = mat[r][c + 1];
+        //         mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
+        //     }
+        // }
         
         return mat;
     }
