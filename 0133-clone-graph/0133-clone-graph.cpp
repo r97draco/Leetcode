912f8205b574402098d58cb90f8220415c05a688
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-02-08 23:09:06 -0600

    Time: 5 ms (75.71%), Space: 8.8 MB (64.23%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -18,34 +18,25 @@ public:
     }
 };
 */
+
+
 class Solution {
-    public:
-    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
-    {
-        vector<Node*> neighbour;
-        Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-            for(auto it:cur->neighbors)
-            {
-                if(mp.find(it)!=mp.end())   //already clone and stored in map
-                {
-                    neighbour.push_back(mp[it]);    //directly push back the clone node from map to neigh
-                }
-                else
-                    neighbour.push_back(dfs(it,mp));
+public:
+    unordered_map<Node*, Node*> mp;
+    Node* dfs(Node* node){
+        if(node == NULL){
+            return NULL;
+        }
+        if(mp.find(node)==mp.end()){
+            mp[node]= new Node(node->val, {});
+            for(auto adj: node->neighbors){
+                mp[node]->neighbors.push_back(dfs(adj));
             }
-            clone->neighbors=neighbour;
-            return clone;
+        }
+        return mp[node];
     }
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*,Node*> mp;
-        if(node==NULL)
-            return NULL;
-        if(node->neighbors.size()==0)   //if only one node present no neighbors
-        {
-            Node* clone= new Node(node->val);
-            return clone; 
-        }
-        return dfs(node,mp);
+        mp.clear();
+        return dfs(node);
     }
 };
