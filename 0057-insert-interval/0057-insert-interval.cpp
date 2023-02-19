310c6bb8a51330e552a17f990608a5a895fa5810
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-02-18 23:57:50 -0600

    Time: 17 ms (64.36%), Space: 17 MB (93.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,65 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size(), i = 0;
         vector<vector<int>> res;
-        int sz= intervals.size();
-        int i=0;
-        while(i < sz && newInterval[0]> intervals[i][1]){
+        //case 1: no overlapping case before the merge intervals
+		//compare ending point of intervals to starting point of newInterval
+        while(i < n && intervals[i][1] < newInterval[0]){
             res.push_back(intervals[i]);
             i++;
-        }
-        
-        while(i < sz && newInterval[1] >= intervals[i][0]){
-            newInterval[0]= min(newInterval[0], intervals[i][0]);
-            newInterval[1]= max(newInterval[1], intervals[i][1]);
+        }                           
+		//case 2: overlapping case and merging of intervals
+        while(i < n && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
             i++;
         }
         res.push_back(newInterval);
-        while(i < sz){
+        // case 3: no overlapping of intervals after newinterval being merged
+        while(i < n){
             res.push_back(intervals[i]);
             i++;
         }
-        
-//         for(int arr=0; arr<intervals.size(); ){
-//         // for(auto arr: intervals){
-//             int k= intervals[arr][0], l=intervals[arr][1];
-//             if(x<k && flag){
-//                 vector<int> tmp(2);
-//                 tmp[0]=x;
-//                 while(y>l && arr<intervals.size()){
-//                     arr++;
-//                     l=intervals[arr][1];
-//                 }
-//                 tmp[1]=l;
-//                 res.push_back(tmp);
-//                 flag=0;
-//                 arr++;
-//             }
-//             else if(x<=l && flag){
-//                 vector<int> tmp(2);
-//                 tmp[0]=k;
-//                 while(y>l && arr<intervals.size()){
-//                     arr++;
-//                     l=intervals[arr][1];
-//                 }
-//                 if(y< intervals[arr][0]){
-//                     tmp[1]=y;
-//                     res.push_back(tmp);
-//                     flag=0;
-//                 }else {
-//                     tmp[1]=l;
-//                     res.push_back(tmp);
-//                     flag=0;
-//                     arr++;
-//                 }
-//             }
-//             else {
-//                 if(arr>= intervals.size())continue;
-//                 res.push_back(intervals[arr]);
-//                 arr++;
-//             }
-            
-//         }
         return res;
     }
 };
