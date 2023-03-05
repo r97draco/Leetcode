7418212fbc8000d927d780a78e8fab4c413c6482
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-03-05 02:23:20 -0600

    Time: 13 ms (66.67%), Space: 2.2 MB (42.98%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.rs
@@ -0,0 +1,20 @@
+use std::cmp;
+
+impl Solution {
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let max = amount + 1;
+        let mut dp = vec![max; (amount + 1) as usize];
+        dp[0] = 0;
+        for i in 1..=amount {
+            for coin in coins.iter() {
+                if (i - coin) >= 0 {
+                    dp[i as usize] = std::cmp::min(dp[i as usize], 1 + dp[(i - coin) as usize]);
+                }
+            }
+        }
+        if dp[amount as usize] == max {
+            return -1;
+        }
+        dp[amount as usize]
+    }
+}
