b4d1327b4d1e23b22ec61422535f75245bc0ed4a
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-03-09 21:45:36 -0600

    Time: 6 ms (37.84%), Space: 11.1 MB (44.52%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int binarySearch(vector<int> &n, int target, int mp){
+        cout<<"hit bS"<<endl;
+        int low, high, mid;
+        if(target >= n[0]){
+            low = 0; high = mp;
+        } else{
+            low= mp; high = n.size()-1;
+        }
+        int res=-1;
+        while(low<= high){
+            mid = (low + high)/2;
+            if(target== n[mid]){
+                return mid;
+            }else if( target< n[mid]){
+                high = mid-1;
+            }
+            else {
+                low = mid +1;
+            }
+        }
+        return res;
+    }
+    int search(vector<int>& n, int target) {
+        int low = 0, high = n.size()-1, mid;
+        int mp=high, res =-1, flag=1;
+        if(n[low]<= n[high]){
+            
+        }
+        else{
+            cout<<"find mp"<<endl;
+            while(low<=high){
+                if(!flag)break;
+                mid= (low + high)/2;
+                if((low== high-1) && n[low]> n[high]){
+                    mp= high;
+                    flag= 0;
+                    break ;
+                }else if(n[low] < n[mid]){
+                    low = mid;
+                }
+                else{
+                    high = mid;
+                }
+            }
+        }
+        cout<< mp<<endl;
+        
+        res = binarySearch(n, target , mp);
+        return res;
+    }
+};
