c192a9a0e348923c04887bd7e9b758bb75948f4f
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-03-13 01:43:52 -0600

    Time: 338 ms (67.12%), Space: 131.2 MB (22.86%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -10,6 +10,8 @@ public:
     }
     
     string get(string key, int timestamp) {
+        //lower_bound returns to the first element that is greater or equal to the key whereas upper_bound returns the first element that is strictly greater than the key.
+//Therefore we always get either the exact element or the one after.
         auto it = ds[key].upper_bound(timestamp);
         return it==ds[key].begin()? "": prev(it)->second;
     }
