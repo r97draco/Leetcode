444f8f0bf68bb12d25ffdd3016073373e66397f1
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-05-28 23:32:40 -0600

    Time: 32 ms (98.75%), Space: 18.5 MB (28.57%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -13,19 +13,12 @@ public:
     void reorderList(ListNode* head) {
         stack<ListNode*> st;
         
-        ListNode* For = head;
         ListNode* forward = head;
-        ListNode* Rev = nullptr;
-        ListNode* Res = new ListNode(-1);
-        ListNode* Ans = Res;
         ListNode* result = new ListNode(-1);
         ListNode* answer = result;
         int count = 0;
         while(head){
-            ListNode* tmp = new ListNode(head->val);
             st.push(head);
-            tmp -> next = Rev;
-            Rev = tmp;
             head= head->next;
             count++;
         }
@@ -33,20 +26,9 @@ public:
         count = count/2;
             
         while(count --){
-            ListNode* tmpa = new ListNode(For->val);
-            For = For->next;
-            ListNode* tmpb = new ListNode(Rev->val);
-            Rev = Rev->next;
-            // cout<<tmpa->val<<" "<<tmpb->val<<" ";
-            Res->next = tmpa;
-            Res = Res->next;
-            Res->next = tmpb;
-            Res = Res->next;
-            
             result->next = forward;
             forward= forward->next;
             result->next->next = st.top();
-            
             result = result->next->next;
             st.pop();
         }
@@ -54,14 +36,6 @@ public:
         result = result->next;}
         result->next = NULL;
         
-        
         head=  answer->next;
-        // ListNode* curr =Ans->next;
-        ListNode* curr =head;
-        while(curr){
-            cout<<curr->val<<" ";
-            curr=curr->next;
-        }
-        
     }
 };
