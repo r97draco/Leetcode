6d95ba0136ec5cd8418e14cf1670b964361d8407
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-07-22 18:21:54 -0600

    Time: 1354 ms (40.37%), Space: 32.6 MB (11.51%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,18 +3,23 @@ class Solution:
         res = [0]* len(temperatures)
         stack  = []
         for index, val in enumerate(temperatures):
-            if not stack:
-                stack.append([val, index])
-            else:
-                item = [val, index]
-                top = stack[-1]
+            while stack and val > stack[-1][0]:
+                stackT, stackI = stack.pop()
+                res[stackI]= index- stackI
+            stack.append([val, index])
+#             if not stack:
+#                 stack.append([val, index])
+#             else:
+#                 item = [val, index]
+#                 top = stack[-1]
                       
-                while stack and item[0]>top[0]:
-                    res[top[1]]= item[1]-top[1]
-                    stack.pop()
-                    if stack:
-                        top = stack[-1]
-                stack.append(item)
+#                 while stack and item[0]>top[0]:
+#                     res[top[1]]= item[1]-top[1]
+#                     stack.pop()
+#                     if stack:
+#                         top = stack[-1]
+#                 stack.append(item)
+
             
         return res
                     
