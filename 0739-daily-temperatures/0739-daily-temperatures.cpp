77e6e4b2fd0fec0b7ab0cd9ef23c76380431cc66
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-07-22 18:36:11 -0600

    Time: 249 ms (20.67%), Space: 102.9 MB (55.48%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<vector<int>> st;
-        vector<int> res(temperatures.size(), 0); // Corrected initialization
-        for (int i = 0; i < temperatures.size(); i++) {
-            while (!st.empty() && temperatures[i] > st.top()[0]) {
-                auto top = st.top();
-                st.pop();
-                res[top[1]] = i - top[1];
-            }
-            st.push({ temperatures[i], i });
+        // stack<vector<int>> st;
+        // vector<int> res(temperatures.size(), 0); // Corrected initialization
+        // for (int i = 0; i < temperatures.size(); i++) {
+        //     while (!st.empty() && temperatures[i] > st.top()[0]) {
+        //         auto top = st.top();
+        //         st.pop();
+        //         res[top[1]] = i - top[1];
+        //     }
+        //     st.push({ temperatures[i], i });
+        // }
+        // return res;
+            stack<int> st;
+    for (int i = 0; i < temperatures.size(); i++) {
+        while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+            int topIndex = st.top();
+            st.pop();
+            temperatures[topIndex] = i - topIndex;
         }
-        return res;
+        st.push(i);
+    }
+    while (!st.empty()) {
+        int topIndex = st.top();
+        st.pop();
+        temperatures[topIndex] = 0; // Remaining elements in the stack have no warmer days
+    }
+    return temperatures;
+
     }
 };
