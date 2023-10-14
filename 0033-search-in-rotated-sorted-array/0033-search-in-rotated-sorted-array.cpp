ddcb8c63088190a7f59dbc4c0d76d3d0538cc1ba
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-14 01:50:39 -0600

    Time: 3 ms (64.17%), Space: 11.3 MB (30.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,15 +5,32 @@ public:
         
         while(l<=r){
             int m = (r-l)/2 + l;
-            double num = (nums[m] < nums[0]) == (target < nums[0])
-                   ? nums[m]
-                   : target < nums[0] ? -INFINITY : INFINITY;
-            if (num < target)
-                l = m + 1;
-            else if (num > target)
-                r = m - 1;
-            else
-                return m;
+            // double num = (nums[m] < nums[0]) == (target < nums[0])
+            //        ? nums[m]
+            //        : target < nums[0] ? -INFINITY : INFINITY;
+            // if (num < target)
+            //     l = m + 1;
+            // else if (num > target)
+            //     r = m - 1;
+            // else
+            //     return m;
+
+            if(target == nums[m])return m;
+
+            // Left sorted part
+            if(nums[l]<= nums[m]){
+                if(target> nums[m] || target<nums[l])
+                    l=m+1;
+                else 
+                    r=m-1;
+            }
+            // Right sorted part
+            else{
+                if(target< nums[m] || target>nums[r])
+                    r=m-1;
+                else 
+                    l=m+1;
+            }
         }
         return -1;
     }
