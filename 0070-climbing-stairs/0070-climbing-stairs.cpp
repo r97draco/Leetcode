305852ae98182a003d926e6faaa1e787f4aba3d1
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-14 14:12:06 -0600

    Time: 3 ms (10.51%), Space: 6.7 MB (7.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    vector<int> dp;
     int climbStairs(int n) {
-        if(n<=3)return n;
-        int dp[n+3];
-        dp[0]=0; dp[1]=1; dp[2]=2;
-        for(int i=3; i<=n; i++){
+        vector<long> dp(n+2, -1);
+        dp[0]= 0;
+        dp[1]= 1;
+        dp[2]= 2;
+        for(int i=3; i<n+2; i++){
             dp[i]= dp[i-1]+dp[i-2];
         }
-        
         return dp[n];
     }
 };
