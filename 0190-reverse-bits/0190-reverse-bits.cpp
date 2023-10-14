cbb66056ad08090ef27efac1c5c43a0751c2b9d7
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-14 03:39:25 -0600

    Time: 0 ms (100%), Space: 6.3 MB (44.81%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        bitset<32> b(n);
-        string s = b.to_string();
-        reverse(s.begin(),s.end());
-        bitset<32> res (s);
-        uint32_t  ans= res.to_ulong();
-        return ans;
+        uint32_t res = 0;
+        for(int i=0; i<32; i++){
+            res = (res<<1) | (n&1);
+            n = n>>1;
+        }
+        return res;
     }
 };
