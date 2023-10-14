e4c1e51829a41a85e1e717d4d39a5730144dbd44
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-14 01:16:31 -0600

    Time: 135 ms (54.13%), Space: 26.3 MB (49.97%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,10 +6,10 @@ public:
         sort(nums.begin(), nums.end());
         for(int i=0; i<sz-2; i++){
             if(i>0 && nums[i]==nums[i-1])continue;
+            if(nums[i]>0)return res;
             int l=i+1, r = sz-1;
             while(l<r){
                 if(nums[i] + nums[l] + nums[r] == 0){
-                    cout<<i<<"-"<<l<<"-"<<r<<endl;
                     vector<int> tmp{nums[i],nums[l],nums[r]};
                     res.push_back(tmp);
                     l++;
