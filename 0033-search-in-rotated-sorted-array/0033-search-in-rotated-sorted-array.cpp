79572aae135031702addaa464c05ed6ebae18436
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-14 01:46:17 -0600

    Time: 0 ms (100%), Space: 11.3 MB (44.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,50 +1,20 @@
 class Solution {
 public:
-    int binarySearch(vector<int> &n, int target, int mp){
-        cout<<"hit bS"<<endl;
-        int low, high, mid;
-        if(target >= n[0]){
-            low = 0; high = mp;
-        } else{
-            low= mp; high = n.size()-1;
-        }
-        int res=-1;
-        while(low<= high){
-            mid = (low + high)/2;
-            if(target== n[mid]){
-                return mid;
-            }else if( target< n[mid]){
-                high = mid-1;
-            }
-            else {
-                low = mid +1;
-            }
-        }
-        return res;
-    }
-    int search(vector<int>& n, int target) {
-        int low = 0, high = n.size()-1, mid;
-        int mp=high, res =-1, flag=1;
-        if(n[low]> n[high]){
-          cout<<"find mp"<<endl;
-            while(low<=high){
-                if(!flag)break;
-                mid= (low + high)/2;
-                if((low== high-1) && n[low]> n[high]){
-                    mp= high;
-                    flag= 0;
-                    break ;
-                }else if(n[low] < n[mid]){
-                    low = mid;
-                }
-                else{
-                    high = mid;
-                }
-            }  
-        }
-        cout<< mp<<endl;
+    int search(vector<int>& nums, int target) {
+        int sz= nums.size(), l = 0, r= sz-1, res=-1;
         
-        res = binarySearch(n, target , mp);
-        return res;
+        while(l<=r){
+            int m = (r-l)/2 + l;
+            double num = (nums[m] < nums[0]) == (target < nums[0])
+                   ? nums[m]
+                   : target < nums[0] ? -INFINITY : INFINITY;
+            if (num < target)
+                l = m + 1;
+            else if (num > target)
+                r = m - 1;
+            else
+                return m;
+        }
+        return -1;
     }
 };
