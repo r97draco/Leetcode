fe1ec45af7ee9d8b8a64d84ecb8f26dbcf5fe4d0
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-14 03:11:02 -0600

    Time: 0 ms (100%), Space: 8.1 MB (49.3%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-       vector<int> result;
-        for(int i=0; i<=n; i++){
-            // bitset<32> c= i;
-            result.push_back(__builtin_popcount(i));
+    int countOne(int n){
+        int count = 0;
+        while(n){
+            count += (n&1);
+            n= n>>1;
         }
-        return result;
+        return count;
+    }
+    vector<int> countBits(int n) {
+       vector<int> res(n+1);
+        for(int i=0; i<= n; i++){
+            res[i]= countOne(i);
+        }
+        return res;
     }
 };
