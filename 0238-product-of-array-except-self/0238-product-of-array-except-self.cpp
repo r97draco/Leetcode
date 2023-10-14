fd8411d1008de905129714e19e81977be7859a2b
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-13 22:07:51 -0600

    Time: 107 ms (6.05%), Space: 24.9 MB (44.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int sz= nums.size();
-        vector<int> pdf(sz, 1), pdb(sz,1);
-        for(int i=0; i< sz-1 ; i++){
-           pdf[i+1]= pdf[i]* nums[i]; 
+        int sz = nums.size();
+        vector<int> ltr (sz, 1);
+        vector<int> rtl (sz, 1);
+
+        for(int i=1; i<sz; i++){
+            ltr[i]= ltr[i-1] * nums[i-1];
+            cout<< ltr[i]<<" ";
         }
-        for(int i=sz-1; i>0 ; i--){
-           pdb[i-1]= pdb[i]* nums[i]; 
+        cout<<endl;
+        for(int i=sz-2; i>=0; i--){
+            rtl[i]= rtl[i+1] * nums[i+1];
+            cout<< rtl[i]<<" ";
         }
-        for(int i=0; i< sz ; i++){
-           pdf[i]= pdf[i]* pdb[i]; 
+        for(int i=0; i<sz; i++){
+            ltr[i]= ltr[i]* rtl[i];
         }
-        return pdf;
+        return ltr;
     }
 };
