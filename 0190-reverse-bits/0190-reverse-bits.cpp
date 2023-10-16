c6c6fe43ea0e5ab71103067327b32d1d3e258777
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-16 13:35:55 -0600

    Time: 4 ms (36.62%), Space: 6.4 MB (31.46%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,11 +3,8 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t res=0;
         for(int i=0; i<32; i++){
-            int x = n&1;
-            res = res<<1;
+            res = res<<1 | n&1;
             n=n>>1;
-            res = res|(x);
-            
         }
         return res;
     }
