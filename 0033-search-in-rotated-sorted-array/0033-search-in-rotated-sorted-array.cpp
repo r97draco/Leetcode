c3fcabb4d357cc228dbd747a59ac24f6d8f62657
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-16 13:12:59 -0600

    Time: 3 ms (64.19%), Space: 11.6 MB (7.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int sz= nums.size(), l = 0, r= sz-1, res=-1;
-        
+        int l=0, r=nums.size()-1;
         while(l<=r){
             int m = (r-l)/2 + l;
-            // double num = (nums[m] < nums[0]) == (target < nums[0])
-            //        ? nums[m]
-            //        : target < nums[0] ? -INFINITY : INFINITY;
-            // if (num < target)
-            //     l = m + 1;
-            // else if (num > target)
-            //     r = m - 1;
-            // else
-            //     return m;
 
-            if(target == nums[m])return m;
-
-            // Left sorted part
-            if(nums[l]<= nums[m]){
-                if(target> nums[m] || target<nums[l])
-                    l=m+1;
-                else 
-                    r=m-1;
+            if(target == nums[m]){
+                return m;
             }
-            // Right sorted part
-            else{
-                if(target< nums[m] || target>nums[r])
-                    r=m-1;
-                else 
-                    l=m+1;
+
+            if(nums[l]<= nums[m]){
+                if( nums[l]>target || nums[m]<target){
+                    l = m+1;
+                }
+                else{
+                    r = m-1;
+                }
+            }
+            else {
+                if( nums[r]<target || nums[m]>target){
+                    r = m-1;
+                }
+                else{
+                    l = m+1;
+                }
             }
         }
         return -1;
