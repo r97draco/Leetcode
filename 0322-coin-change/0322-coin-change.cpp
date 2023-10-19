7e3b6c2bad7728df4abc374c3ef7c2060326e9a1
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-19 01:45:40 -0600

    Time: 72 ms (60.15%), Space: 15 MB (54.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,14 +3,11 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1, INT_MAX);
         dp[0]=0;
-        // sort(coins.begin(), coins.end());
+        sort(coins.begin(), coins.end());
         for(int i=1; i<= amount; i++){
             int amt = i;
             for(auto coin : coins){
                 if(coin> amt)continue;
-                // if(dp[i-1]+ coin == amt){
-                //     int ls = dp[i-1]+1;
-                // }
                 if(dp[amt-coin]!= INT_MAX)
                 dp[i]= min(dp[amt-coin]+1, dp[i]);
             }
