0605351f6e92ed92428b56d1b349a942481e2fda
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-19 02:01:35 -0600

    Time: 10 ms (55.68%), Space: 2.2 MB (51.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.rs
+++ 0322-coin-change/0322-coin-change.rs
@@ -1,17 +1,21 @@
 impl Solution {
     pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
-        let mut changes = vec![amount+1; (amount+1) as usize];
-        changes[0] = 0;
-        for i in 1..(amount+1) as usize {
-            for &coin in &coins {
-                let coin = coin as usize;
-                if coin > i {continue};
+        let max = i32::MAX;
+        let mut dp = vec![max; (amount + 1) as usize];
+        dp[0] = 0;
 
-                changes[i] = changes[i].min(changes[i-coin] + 1);
+        for i in 1..=amount as usize {
+            for &coin in &coins { // change here, borrowing 'coins' for iteration
+                if coin as usize <= i && dp[i - coin as usize] != max {
+                    dp[i] = dp[i].min(dp[i - coin as usize] + 1);
+                }
             }
         }
 
-        let change = changes[amount as usize];
-        if change == amount+1 {-1} else {change}
+        if dp[amount as usize] == max { 
+            -1 
+        } else { 
+            dp[amount as usize] 
+        }
     }
 }
