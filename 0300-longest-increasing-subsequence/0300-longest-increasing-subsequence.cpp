d1e22c0caf18013c6857dd8472bfbd44de934f44
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2023-10-20 22:09:41 -0600

    Time: 230 ms (65.4%), Space: 10.9 MB (41.83%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for(int i=0; i<n; i++)
-            for(int j=0; j<i; j++)
-                if(nums[i]> nums[j] && dp[i]< dp[j]+1)
-                    dp[i]= dp[j]+1;
-        return *max_element(dp.begin(),dp.end());    
-     }
-}; 
+    int lengthOfLIS(vector<int>& nums) {
+        int sz= nums.size();
+        vector<int> dp(sz+1, 1);
+        for(int i=0; i<sz; i++){
+            for(int j=0; j<i; j++){
+                if( nums[i]> nums[j] && dp[i]<dp[j]+1 )
+                 dp[i]=dp[j]+1;
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
