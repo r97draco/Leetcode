d4ed9e38661da8130a2c7bbc11a8413981a2e484
Jagroop Singh <59063098+r97draco@users.noreply.github.com>
2024-05-18 14:09:15 -0600

    Time: 2 ms (48.71%), Space: 6.9 MB (99.79%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t res=0;
-        for(int i=0; i<32; i++){
-            res = res<<1 | n&1;
-            n=n>>1;
+        uint32_t reversed = 0;
+        for(uint32_t i= 0; i<32; i++){
+            int bit = 0;
+            if((n & (1<<i)) !=0){
+                bit =1;
+            }
+
+            // Shift reversed left to make space for the new bit
+            reversed <<= 1;
+
+            // Set the last bit of reversed to the current bit
+            reversed |= bit;
         }
-        return res;
+        return reversed;
     }
 };
